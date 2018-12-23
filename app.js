4d3fdc6468995db96bbf6fa1e4cc81d61f01822d
DevCyberish <46059292+DevCyberish@users.noreply.github.com>
2018-12-23 16:33:32 -0500

    skid master

---
+++ app.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const chalk = require('chalk');
+const request = require('request');
+const config = require('./config.json');
+
+client.on("ready", () => {
+  console.log(chalk.blue("HI"));
+  client.user.setActivity('over the network', { type: 'WATCHING' })
+});
+
+client.on("message", async message => {
+
+  let prefix = config.prefix;
+
+  if(message.content === prefix + "settings") {
+    let embed = new Discord.RichEmbed()
+      .setColor(0x00FFFF)
+      .setAuthor(client.user.username, client.user.displayAvatarURL)
+      .setFooter(message.author.username, message.author.displayAvatarURL)
+      .addField('Prefix', '**a!**');
+
+    message.channel.send(embed);
+    console.log("settings command has used");
+  }
+
+});
+client.login("NTI2NTA1MTE1OTgxNTc4MjQw.DwGMaQ.eOCLad5F_X3LZqIbyqfTBd6JV1s");
